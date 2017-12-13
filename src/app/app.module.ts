76bd4dc5c9f57aa7fbf61dd4f6c2bbcd9dfa037e
jacobdlewis <jacobdlewis@gmail.com>
2017-12-13 12:47:40 -0600

    Used angular cli to generate search, character list, character list item, and pagination components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { CharacterListItemComponent } from './character-list-item/character-list-item.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    CharacterListComponent,
+    CharacterListItemComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/character-list-item/character-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-character-list-item',
+  templateUrl: './character-list-item.component.html',
+  styleUrls: ['./character-list-item.component.css']
+})
+export class CharacterListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
