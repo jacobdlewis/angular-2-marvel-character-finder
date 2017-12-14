42db16377be81ecb1e7c76f39480dd99a3b980ab
jacobdlewis <jacobdlewis@gmail.com>
2017-12-14 11:09:27 -0600

    Fleshed out character details component.

--- src/app/character-details/character-details.component.ts
+++ src/app/character-details/character-details.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CharactersService } from '../characters.service';
 
 @Component({
   selector: 'app-character-details',
   templateUrl: './character-details.component.html',
   styleUrls: ['./character-details.component.css']
 })
-export class CharacterDetailsComponent implements OnInit {
+export class CharacterDetailsComponent implements OnDestroy, OnInit {
+  character;
+  activatedRoute: ActivatedRoute;
+  charactersService: CharactersService;
+  subscription;
 
-  constructor() { }
+  constructor(activatedRoute: ActivatedRoute, charactersService: CharactersService) {
+    this.activatedRoute = activatedRoute;
+    this.charactersService = charactersService;
+  }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(
+      (params) => {
+        this.charactersService.searchCharacter(params.id);
+      }
+    );
+    this.subscription = this
+    .charactersService
+    .characterUpdated
+    .subscribe(
+      () => {
+        this.character = this.charactersService.getCharacter();
+        console.log('this character', this.character);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/characters.service.ts
+++ src/app/characters.service.ts
@@ -12,13 +12,15 @@ export class CharactersService {
   // how can I have a Character type?
   // I think TS has a 'rubric/blueprint' feature for this
   private characters = [];
+  private character;
   charactersUpdated = new Subject<void>();
+  characterUpdated = new Subject<void>();
 
   constructor(http: Http) {
     this.http = http;
   }
 
-  generateQueryParams(query) {
+  generateQueryParams(query = {}) {
     return stringify({
       apikey: PUBLIC_KEY,
       ...query
@@ -27,6 +29,33 @@ export class CharactersService {
 
   getCharacters() {
     return this.characters;
+  }
+
+  getCharacter() {
+    return this.character;
+  }
+
+  searchCharacter(id) {
+    const params = this.generateQueryParams();
+    this.http
+      .get(`http://gateway.marvel.com/v1/public/characters/${id}?${params}`)
+      .map((response: Response) => {
+        const data = response.json().data;
+        return data;
+      })
+      .subscribe(
+        (data) => {
+          console.log('character search success', data);
+          this.character = data.results[0];
+          this.characterUpdated.next();
+        },
+        (error) => {
+          console.log('character search error', error);
+        },
+        () => {
+          console.log('search character response ended');
+        }
+      );
   }
 
   searchCharacters(query) {
@@ -39,12 +68,12 @@ export class CharactersService {
       })
       .subscribe(
         (data) => {
-          console.log('character search success', data);
+          console.log('characters search success', data);
           this.characters = data.results;
           this.charactersUpdated.next();
         },
         (error) => {
-          console.log('character search error', error);
+          console.log('characters search error', error);
         },
         () => {
           console.log('search characters response ended');
